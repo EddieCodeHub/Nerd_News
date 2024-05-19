bbb1040290531ac159be3910be193f3d7a057670
EddieCodeHub Meekin <eddiemeekincode@gmail.com>
2024-05-19 12:20:01 +0000

    feat: add better functionality to admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import News_Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
+@admin.register(News_Post)
+class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(News_Post)
 admin.site.register(Comment)
