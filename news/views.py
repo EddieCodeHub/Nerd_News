ec8969154f856554ecb4b4212a149f28837e56e3
EddieCodeHub Meekin <eddiemeekincode@gmail.com>
2024-05-21 15:34:00 +0000

    feat: begin to impliment post_detail view

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import News_Post, Comment
 
@@ -8,3 +8,27 @@ class news_post_list(generic.ListView):
     queryset = News_Post.objects.all()
     template_name = "news/index.html"
     paginate_by = 3
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`news.News_Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`news.News_Post`.
+
+    **Template:**
+
+    :template:`news/post_detail.html`
+    """
+
+    queryset = News_Post.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "news/post_detail.html",
+        {"News_Post": post},
+    )
