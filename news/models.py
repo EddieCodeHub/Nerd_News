a73537a1bfdbd54cba21190b6a4a61d20f205e72
Edward Meekin <eddiemeekincode@gmail.com>
2024-06-23 10:47:27 +0000

    fest: attempt to impleiment post summary on news-post cards

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
 
 
 
@@ -11,6 +12,7 @@ class News_Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="News_posts")
     content = models.TextField()
+    summary = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=1)
@@ -20,6 +22,11 @@ class News_Post(models.Model):
     
     def __str__(self):
         return f"{self.title} written by {self.author}"
+    
+    def save(self, *args, **kwargs):
+        if not self.summary:
+            self.summary = Truncator(self.content).chars(100)
+        super(News_Post, self).save(*args, **kwargs)
 
 
 class Comment(models.Model):
