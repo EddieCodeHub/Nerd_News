d8568ce622eae91095e184bf79436856fc6c17f0
Edward Meekin <eddiemeekincode@gmail.com>
2024-08-13 17:13:01 +0000

    docs: change secret key in settings

--- nerdnews/settings.py
+++ nerdnews/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1&8kz=b5l--h-&7n$pf-&7injv(_=eubtq0%8=h8_yp187fr4a'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
